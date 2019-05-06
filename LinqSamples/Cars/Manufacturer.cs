62a994970bc8d006cdb3b9f415c879ed8e72265b
Azeez Olusegun Odumosu <az6bcn@gmail.com>
2019-05-06 17:35:42 +0100

    Joining, Grouping and Aggregating

---
+++ LinqSamples/Cars/Manufacturer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Cars
+{
+    public class Manufacturer
+    {
+        public string Name { get; set; }
+        public string Headquarters { get; set; }
+        public int Year { get; set; }
+
+        public static Manufacturer ParseFromCSV(string line)
+        {
+            var columns = line.Split(',');
+
+            return new Manufacturer
+            {
+                Name = columns[0],
+                Headquarters = columns[1],
+                Year = int.Parse(columns[2]),
+            };
+        }
+    }
+}
--- LinqSamples/Cars/Program.cs
+++ LinqSamples/Cars/Program.cs
@@ -9,19 +9,42 @@ namespace Cars
     {
         static void Main(string[] args)
         {
+            // read csv file into memory
             var cars = ProcessFile("fuel.csv");
+            var manufactures = ProcessManufacturesFile("manufacturers.csv");
 
             var mostEfficientCars = MostFuelEfficientCar(cars, 2 , 10);
             foreach (var car in mostEfficientCars)
             {
-                Console.WriteLine($"{car.Name} : {car.Combined}");
+                //Console.WriteLine($"{car.Name} : {car.Combined}");
             }
 
             var topBMWEfficientCar2016 = FilterBySpecificManufacturerForYear2016("BMW", cars).First();
-            Console.WriteLine($"BMW most Efficient Car 2016: {topBMWEfficientCar2016.Name} : {topBMWEfficientCar2016.Combined}");
+            //Console.WriteLine($"BMW most Efficient Car 2016: {topBMWEfficientCar2016.Name} : {topBMWEfficientCar2016.Combined}");
 
             var anoymousType = FilterBySpecificManufacturerForYear2016Projection("BMW", cars);
-            Console.WriteLine($"BMW most Efficient Car 2016: {anoymousType.Maker} : {anoymousType.MadeInYear}");
+            //Console.WriteLine($"BMW most Efficient Car 2016: {anoymousType.Maker} : {anoymousType.MadeInYear}");
+
+
+            // **************************************** J o i n *************************************
+            var top10EfficientCarsAndHeadquarters = GetMostEfficientCarAndManufacturersHeadquarter(cars, manufactures);
+            foreach (var item in top10EfficientCarsAndHeadquarters)
+            {
+                Console.WriteLine($"{item.Vehile}: {item.HeadQuaters}");
+            }
+
+
+            // **************************************** G r  o u p J o  i n **************************
+            var group = GetCarsManufacturersInEachHeadquarter(cars, manufactures);
+            foreach (var item in group)
+            {
+                Console.WriteLine($" Maker => {item.Maker}, Headquarters => {item.HeadQuaters}");
+                // iterate the list of each group: maker
+                foreach (var car in item.ListOfCars)
+                {
+                    Console.WriteLine(($"{car.Name} : {car.Manufacture}"));
+                }
+            }
         }
 
         private static List<Car> ProcessFile(string path)
@@ -32,6 +55,18 @@ namespace Cars
                         .Select(Car.ParseFromCSV)
                         .ToList();
         }
+
+
+        private static List<Manufacturer> ProcessManufacturesFile(string path)
+        {
+            return File.ReadLines(path)
+                        .Where(line => line.Length > 1)
+                        .Select(Manufacturer.ParseFromCSV) // pass each line to the method to project its information into Manufacture object
+                        .ToList();
+        }
+
+
+
 
         /// <summary>
         /// Mosts the fuel efficient car. i.e car that emits least (Combined)
@@ -76,6 +111,42 @@ namespace Cars
                             MadeInYear = c.Year
                         })
                         .First();
+        }
+
+
+        // ######################### J o i n #################
+
+        private static dynamic GetMostEfficientCarAndManufacturersHeadquarter( List<Car> cars, List<Manufacturer> manufacturers)
+        {
+            return cars
+                .OrderByDescending( e => e.Combined)
+                .Join(manufacturers,
+                c => c.Manufacture, //key on the left side (1st entity: Cars)
+                m => m.Name, //key on the right side (2nd entity: Manufactures)
+                (car, marker) => new //result selector: what to do once matched: projection result
+                {
+                    Vehile = car.Name,
+                    HeadQuaters = marker.Headquarters
+                }).ToList();
+        }
+
+        // GroupJoin by marker name: group car by their makers
+        private static dynamic GetCarsManufacturersInEachHeadquarter(List<Car> cars, List<Manufacturer> manufacturers)
+        {
+            var xxx = manufacturers
+                .GroupJoin(cars,
+                m => m.Name, //key on the left side (1st entity: Manufactures)
+                c => c.Manufacture, //key on the right side (2nd entity: Cars)
+                (maker, carsByMaker) => new //result selector: projection result
+                {
+                    Maker = maker.Name,
+                    HeadQuaters = maker.Headquarters,
+                    ListOfCars = carsByMaker,  // list/group of cars for each marker 
+                })
+                .Take(5)
+                .ToList();
+
+            return xxx;
         }
     }
 }
